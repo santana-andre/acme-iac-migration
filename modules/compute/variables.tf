494a769f9de91b479ad6600710cbf5f8d5d1ebdd
André Santana <andre@desantana.com.br>
2025-08-25 12:19:10 -0300

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,10 +1,25 @@
-variable "vpc_id" { type = string }
-variable "public_subnet_id" { type = string }
-variable "instance_type" { type = string }
-variable "tags" { type = map(string) }
-variable "demo_message" {
+variable "vpc_id" {
+  description = "VPC onde os recursos de compute serão criados"
   type        = string
-  default     = "Hello ACME - IaC OK (Ubuntu)"
-  description = "Mensagem exibida na página inicial do Apache"
 }
 
+variable "public_subnet_id" {
+  description = "Subnet pública onde a instância ficará"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Tipo da instância EC2 (ex.: t2.micro)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Mapa de tags padrão aplicado a todos os recursos"
+  type        = map(string)
+}
+
+variable "demo_message" {
+  description = "Mensagem exibida no HTML da página inicial"
+  type        = string
+  default     = "Hello ACME - IaC OK (Ubuntu)"
+}
