c7168bc8de41beb903af468ea957a7b9ef30fbf7
André Santana <andre@desantana.com.br>
2025-08-25 11:28:20 -0300

    Update variables.tf

--- live/variables.tf
+++ live/variables.tf
@@ -1,3 +1,9 @@
 variable "aws_region"   { type = string }
 variable "cidr_block"   { type = string }
 variable "global_tags"  { type = map(string) }
+variable "demo_message" {
+  type        = string
+  default     = "Hello ACME - IaC OK (Ubuntu)"
+  description = "Mensagem exibida na página inicial do Apache"
+}
+
