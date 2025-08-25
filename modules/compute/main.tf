33a32b123028880a0187a818db40f7b7fa1b4308
André Santana <andre@desantana.com.br>
2025-08-25 11:06:08 -0300

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -16,18 +16,19 @@ resource "aws_security_group" "web" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 us-east-1
-  instance_type = var.instance_type
-  subnet_id     = var.public_subnet_id
+  ami                    = "ami-053b0d53c279acc90" # Ubuntu 22.04 us-east-1
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.web.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
-              echo "<h1>Hello ACME - IaC OK</h1>" > /var/www/html/index.html
-            EOF
-  tags      = var.tags
+                #!/bin/bash
+                apt-get update -y
+                apt-get install -y apache2
+                systemctl start apache2
+                systemctl enable apache2
+                echo "<h1>Hello ACME - IaC OK (Ubuntu)</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = var.tags
 }
