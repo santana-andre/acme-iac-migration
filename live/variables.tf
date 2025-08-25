ba698d451c5bb1d4e0edb265c6b15c6fef77636a
André Santana <andre@desantana.com.br>
2025-08-25 14:10:08 -0300

    Update variables.tf

--- live/variables.tf
+++ live/variables.tf
@@ -1,6 +1,15 @@
 variable "aws_region"   { type = string }
 variable "cidr_block"   { type = string }
-variable "global_tags"  { type = map(string) }
+variable "global_tags" {
+  type = map(string)
+  validation {
+    condition = contains(keys(var.global_tags), "Owner")
+            && contains(keys(var.global_tags), "CostCenter")
+            && contains(keys(var.global_tags), "Project")
+            && contains(keys(var.global_tags), "Env")
+    error_message = "global_tags must include Owner, CostCenter, Project and Env."
+  }
+}
 variable "demo_message" {
   type        = string
   default     = "Hello ACME - IaC OK (Ubuntu)"
