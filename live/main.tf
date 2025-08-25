53d83d4576bc308e710e1d40ee45cac328a1bee0
André Santana <andre@desantana.com.br>
2025-08-24 23:37:10 -0300

    Create main.tf

---
+++ live/main.tf
@@ -0,0 +1,20 @@
+module "network" {
+  source     = "../modules/network"
+  cidr_block = var.cidr_block
+  tags       = var.global_tags
+}
+
+module "compute" {
+  source           = "../modules/compute"
+  vpc_id           = module.network.vpc_id
+  public_subnet_id = module.network.public_subnet_id
+  instance_type    = "t2.micro"
+  tags             = var.global_tags
+  user_data        = <<-EOF
+                      #!/bin/bash
+                      yum update -y
+                      yum install -y httpd
+                      echo "Hello ACME - IaC OK" > /var/www/html/index.html
+                      systemctl enable httpd --now
+                      EOF
+}
