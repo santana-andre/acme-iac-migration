1dcd73909a9731f6a57060a3f6f7eea47ac4b0f4
André Santana <andre@desantana.com.br>
2025-08-25 11:57:29 -0300

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,7 +1,6 @@
 variable "vpc_id" { type = string }
 variable "public_subnet_id" { type = string }
 variable "instance_type" { type = string }
-variable "user_data" { type = string }
 variable "tags" { type = map(string) }
 variable "demo_message" {
   type        = string
