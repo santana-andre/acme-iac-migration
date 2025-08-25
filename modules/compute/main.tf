1fc152429dc492727a467bf9d1fc47977d620fdb
André Santana <andre@desantana.com.br>
2025-08-25 10:47:00 -0300

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -23,10 +23,10 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
+              apt-get update -y
+              apt-get install -y apache2
+              systemctl start apache2
+              systemctl enable apache2
               echo "<h1>Hello ACME - IaC OK</h1>" > /var/www/html/index.html
             EOF
   tags      = var.tags
