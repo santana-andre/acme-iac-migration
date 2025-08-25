b68073fa4589adb94553e350e13f125bbdc40055
André Santana <andre@desantana.com.br>
2025-08-25 11:11:18 -0300

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -3,3 +3,9 @@ variable "public_subnet_id" { type = string }
 variable "instance_type" { type = string }
 variable "user_data" { type = string }
 variable "tags" { type = map(string) }
+variable "demo_message" {
+  type        = string
+  default     = "Hello ACME - IaC OK (Ubuntu)"
+  description = "Mensagem exibida na página inicial do Apache"
+}
+
