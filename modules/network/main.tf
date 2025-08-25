a50a3497bcaf785b74b0604319ce3bdd03e91e7b
André Santana <andre@desantana.com.br>
2025-08-24 23:34:46 -0300

    Create main.tf

---
+++ modules/network/main.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+  tags       = var.tags
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 0)
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+  tags   = var.tags
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
