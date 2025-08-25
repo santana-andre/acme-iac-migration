8ecb48e3e99f65af6135d1ca00aa3aa725464ce9
André Santana <andre@desantana.com.br>
2025-08-25 11:54:26 -0300

    Update main.tf

--- live/main.tf
+++ live/main.tf
@@ -10,6 +10,7 @@ module "compute" {
   public_subnet_id = module.network.public_subnet_id
   instance_type    = "t2.micro"
   tags             = var.global_tags
+  demo_message      = var.demo_message
   user_data        = <<-EOF
                       #!/bin/bash
                       yum update -y
